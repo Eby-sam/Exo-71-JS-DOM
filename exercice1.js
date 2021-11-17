79b4322b32faed78c90f9606bac1cb477ba533b9
coque <coquelet.samuel@gmail.com>
2021-11-17 16:47:04 +0100

    fini

--- exercice1.js
+++ exercice1.js
@@ -1,7 +1,13 @@
-let monDiv = .createElement('div');
-
+let monDiv = document.createElement('div');
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
+let monDiv2 = document.createElement('div');
 document.getElementById('viewport').appendChild(monDiv);
-
+monDiv2.style.width = "100px";
+monDiv2.style.height= "100px";
+monDiv2.style.backgroundColor = "red";
+document.getElementById('viewport').appendChild(monDiv2);
+let span1 = document.getElementById("aSupprimer");
+span1.remove();
 
 
 
